9c9cf74420bfecd40245659f28f59c754ac8c2b7
kmj1004 <49183841+kmj1004@users.noreply.github.com>
2019-05-16 21:03:44 +0900

    Add files via upload

--- search.php
+++ search.php
@@ -3,20 +3,20 @@
   $search = $_POST['search'];
   $sql = "SELECT * FROM board
           WHERE title LIKE '%$search%'
-          AND contents LIKE '%$search%'";
+          OR contents LIKE '%$search%'";
   $result = mysqli_query($conn, $sql);
 
   $list = '';
   while($row = mysqli_fetch_array($result)){
        $list .= "<a href=\"update.php?title={$row['title']}\">{$row['title']}</a><br>";
      }
-  echo $list;
 
 ?>
 
 <!DOCTYPE html>
 <html>
   <body>
+    <?= $list ?>
     <form action="index.php" method="post">
       <br><input type="submit" value="돌아가기"></br>
     </form>
