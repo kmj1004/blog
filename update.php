262bd36f2c7c76da8857a00478bbcc914790b175
kmj1004 <49183841+kmj1004@users.noreply.github.com>
2019-06-02 20:08:01 +0900

    Add files via upload

--- update.php
+++ update.php
@@ -12,6 +12,7 @@ $user_name = $_SESSION['user_name'];
      break;
    }
  }
+
  $filetype = strtolower(pathinfo($row['image'], PATHINFO_EXTENSION));
  $imagetype = array('jpg', 'png');
  if(in_array($filetype, $imagetype)) {
@@ -19,7 +20,7 @@ $user_name = $_SESSION['user_name'];
  } else {
    $file = $row['image'];
  }
- //echo "1";
+ list($width, $height) = getimagesize($file);
 ?>
 
 <!DOCTYPE html>
@@ -36,28 +37,19 @@ $user_name = $_SESSION['user_name'];
     <h1><?= $user_name ?>님의 블로그</h1>
 
     <form action = "process_update.php" method="post">
-      <input type="text" placeholder="제목" name="title" value="<?=$row['title']?>" style="width: 200px; ">
+      <input type="text" placeholder="제목" name="title" value="<?=$row['title']?>" style="width: 200px;">
       <input type="file" name="upload" style="width:200px;">
-      <a href="<?=basename('$file')?>" download="<?=$file?>">
-
-        <img src="<?=$row['image']?>"></a>
-
-
-<!--       <div contenteditable="true">
-        <img src="<?=$row['image']?>">
-        <textarea name="contents"><?=$row['contents']?></textarea>
-      </div>
-     <div style="display:inline; ">
-        <textarea name="contents" style="width:400px; height:200px;">
-          <img src="<?=$row['image']?>"
-          <?=$row['contents']?></textarea>
-      </div> -->
+      <p><a href="<?=$file?>" download="<?=basename("$file")?>">
+      <img src="<?= $file ?>"></a>
+      <textarea name="contents" style="width:<?=$width?>px; height:<?=$height?>px;"><?=$row['contents']?></textarea></p>
 
       <input type ="hidden" name="board_id" value="<?=$row['board_id']?>">
       <input type="submit" value="수정">
     </form>
     <form action="delete.php" method="post">
       <input type ="hidden" name="board_id" value="<?=$row['board_id']?>">
+      <input type="hidden" name="file"
+      value="<?=$file?>">
       <input type="submit" value="삭제">
     </form>
     <form action="main.php" method="post">
