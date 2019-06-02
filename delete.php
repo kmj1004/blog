38ec9ac094584e9aeece179ca5889bcb9f69fc4e
kmj1004 <49183841+kmj1004@users.noreply.github.com>
2019-06-02 19:00:14 +0900

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,15 +1,16 @@
 <?php
   include 'dbconnect.php';
-
   $sql = "DELETE FROM board WHERE
     board_id = {$_POST['board_id']}";
   $result = mysqli_query($conn, $sql);
-
-  //unlink('{$_POST['file']}');
+  unlink("{$_POST['file']}");
   if($result === false) {
   echo "<script>alert('문제가 생겼습니다. 관리자에게 문의해주세요'); history.back(); </script>";
   print_r(mysqli_error($conn));
-} else {
+  } else {
   echo "<script>alert('삭제되었습니다.'); location.href='main.php'; </script>";
-}
+  }
+
+
+
  ?>
